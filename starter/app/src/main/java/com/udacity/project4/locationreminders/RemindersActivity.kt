41f49821d967988014d24d37f5163aa9ddd7654e
fwornle <fwornle@yahoo.co.uk>
2021-10-28 07:50:09 +0200

    upgraded gradle plugin // updated dependencies to latest libs // replaced deprecated synthetics

--- starter/app/src/main/java/com/udacity/project4/locationreminders/RemindersActivity.kt
+++ starter/app/src/main/java/com/udacity/project4/locationreminders/RemindersActivity.kt
@@ -7,25 +7,35 @@ import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
 import com.udacity.project4.R
-import kotlinx.android.synthetic.main.activity_reminders.*
+import com.udacity.project4.databinding.ActivityRemindersBinding
 
 /**
  * The RemindersActivity that holds the reminders fragments
  */
 class RemindersActivity : AppCompatActivity() {
 
+    // Contains all the views
+    private lateinit var binding: ActivityRemindersBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_reminders)
+
+
+        // Inflate the layout
+        binding = DataBindingUtil.setContentView<ActivityRemindersBinding>(this, R.layout.activity_reminders)
+        setContentView(binding.root)
 
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         when (item.itemId) {
             android.R.id.home -> {
-                (nav_host_fragment as NavHostFragment).navController.popBackStack()
+                binding.navHostFragment.findNavController().popBackStack()
                 return true
             }
         }
