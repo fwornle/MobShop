955438b79f60c0a58e434ae5583336ca73cdb2b6
fwornle <fwornle@yahoo.co.uk>
2021-11-13 11:24:35 +0100

    map initialized, permission handling (user location), fly to user location

--- starter/app/src/main/java/com/udacity/project4/locationreminders/savereminder/SaveReminderFragment.kt
+++ starter/app/src/main/java/com/udacity/project4/locationreminders/savereminder/SaveReminderFragment.kt
@@ -13,19 +13,25 @@ import com.udacity.project4.utils.setDisplayHomeAsUpEnabled
 import org.koin.android.ext.android.inject
 
 class SaveReminderFragment : BaseFragment() {
-    // get the view model this time as a single to be shared with the another fragment
+
+    // get the view model (from Koin) this time as a singleton to be shared with another fragment
     override val _viewModel: SaveReminderViewModel by inject()
+
+    // data binding of underlying layout
     private lateinit var binding: FragmentSaveReminderBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
+
+        // inflate fragment layout and return binding object
         binding =
             DataBindingUtil.inflate(inflater, R.layout.fragment_save_reminder, container, false)
 
         setDisplayHomeAsUpEnabled(true)
 
+        // provide (injected) viewModel as data source for data binding
         binding.viewModel = _viewModel
 
         return binding.root
@@ -34,12 +40,18 @@ class SaveReminderFragment : BaseFragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding.lifecycleOwner = this
+
+        // clicking on the 'selectLocation' textView takes you to the fragment "select location"
+        // ... by means of the observer function of MutableLiveData element 'navigationCommand'
+        //     --> see BaseFragment.kt... where the observer (lambda) is installed
         binding.selectLocation.setOnClickListener {
             //            Navigate to another fragment to get the user location
             _viewModel.navigationCommand.value =
                 NavigationCommand.To(SaveReminderFragmentDirections.actionSaveReminderFragmentToSelectLocationFragment())
         }
 
+        // clicking on the 'saveReminder' FAB...
+        // ... installs the geofencing request and triggers the saving to DB
         binding.saveReminder.setOnClickListener {
             val title = _viewModel.reminderTitle.value
             val description = _viewModel.reminderDescription
