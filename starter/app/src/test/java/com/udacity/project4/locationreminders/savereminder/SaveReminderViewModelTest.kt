e7aa0e322d2870217f1156042d251e42ffcb13ea
fwornle <fwornle@yahoo.co.uk>
2021-11-28 11:05:10 +0100

    unit test running, reflection used to access private methods to be tested

--- starter/app/src/test/java/com/udacity/project4/locationreminders/savereminder/SaveReminderViewModelTest.kt
+++ starter/app/src/test/java/com/udacity/project4/locationreminders/savereminder/SaveReminderViewModelTest.kt
@@ -9,9 +9,9 @@ import com.udacity.project4.locationreminders.reminderslist.ReminderDataItem
 import com.udacity.project4.locationreminders.reminderslist.RemindersListViewModel
 
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-import org.junit.After
 import org.junit.Assert
 import org.junit.Before
+import org.junit.BeforeClass
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.koin.android.ext.koin.androidContext
@@ -19,112 +19,149 @@ import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.core.context.startKoin
 import org.koin.core.context.stopKoin
 import org.koin.dsl.module
-import org.koin.test.KoinTest
+import org.koin.test.AutoCloseKoinTest
 import org.koin.test.inject
+import java.lang.reflect.Method
 
 @ExperimentalCoroutinesApi
 @RunWith(AndroidJUnit4::class)
-class SaveReminderViewModelTest: KoinTest {
+class SaveReminderViewModelTest: AutoCloseKoinTest() {
 
+    // declare globally used variables
+    private lateinit var reminderData: ReminderDataItem
+    private lateinit var privateTestFun: Method
+
+    private var testInitialized = false
+    private val _viewModel: SaveReminderViewModel by inject()
+
+    // run before every test
     @Before
-    fun setup() {
+    fun setupClass() {
 
-        // stop the original app koin, which is launched when the application starts (in "MyApp")
-        stopKoin()
+        // run once, ab beginning of test suite  ----------------------------------------
 
-        /**
-         * use Koin Library as a service locator
-         */
-        val myModule = module {
+        // ... done this way to avoid hassle with @BeforeClass & @JvmStatic when using DI
+        //     see: https://stackoverflow.com/questions/32952884/junit-beforeclass-non-static-work-around-for-spring-boot-application
+        if (testInitialized) {
 
-            // declare a ViewModel - to be injected into Fragment with dedicated injector using
-            // "by viewModel()"
-            //
-            // class RemindersListViewModel(
-            //    app: Application,
-            //    private val dataSource: ReminderDataSource
-            // ) : BaseViewModel(app) { ... }
-            viewModel {
-                RemindersListViewModel(
-                    get(),  // app (context)
-                    get() as ReminderDataSource  // repo as data source
-                )
+            // stop the original app koin, which is launched when the application starts (in "MyApp")
+            stopKoin()
+
+            /**
+             * use Koin Library as a service locator
+             */
+            val myModule = module {
+
+                // declare a ViewModel - to be injected into Fragment with dedicated injector using
+                // "by viewModel()"
+                viewModel {
+                    RemindersListViewModel(
+                        get(),  // app (context)
+                        get() as ReminderDataSource  // repo as data source
+                    )
+                }
+
+                // declare a ViewModel - to be injected into Fragment with standard injector using
+                // "by inject()"
+                // --> this view model is declared singleton to be used across multiple fragments
+                single {
+                    SaveReminderViewModel(
+                        get(),
+                        get() as ReminderDataSource
+                    )
+                }
+
+                // declare a (singleton) repository service with interface "ReminderDataSource"
+                // note: the repo needs the DAO of the room database (RemindersDao)
+                //       ... which is why it is declared (below) as singleton object and injected
+                //           using "get()" in the lambda of the declaration
+                single<ReminderDataSource> { RemindersLocalRepository(get()) }
+
+                // declare the local DB singleton object - used as data source for the repository
+                // note: LocalDB.createRemindersDao returns a DAO with interface RemindersDao
+                //       ... the DAO is needed by the repo (where it is injected, see "get()", above)
+                single { LocalDB.createRemindersDao(ApplicationProvider.getApplicationContext()) }
             }
 
-            // declare a ViewModel - to be injected into Fragment with standard injector using
-            // "by inject()"
-            // --> this view model is declared singleton to be used across multiple fragments
-            //
-            // class SaveReminderViewModel(
-            //    val app: Application,
-            //    val dataSource: ReminderDataSource
-            // ) : BaseViewModel(app) { ... }
-            single {
-                SaveReminderViewModel(
-                    get(),
-                    get() as ReminderDataSource
-                )
+            // instantiate viewModels, repos and DBs and inject them as services into consuming classes
+            // ... using KOIN framework (as "service locator"): https://insert-koin.io/
+            startKoin {
+                androidContext(ApplicationProvider.getApplicationContext())
+                modules(listOf(myModule))
             }
 
-            // declare a (singleton) repository service with interface "ReminderDataSource"
-            // note: the repo needs the DAO of the room database (RemindersDao)
-            //       ... which is why it is declared (below) as singleton object and injected
-            //           using "get()" in the lambda of the declaration
-            //
-            // class RemindersLocalRepository(
-            //    private val remindersDao: RemindersDao,
-            //    private val ioDispatcher: CoroutineDispatcher = Dispatchers.IO
-            //) : ReminderDataSource { ... }
-            single<ReminderDataSource> { RemindersLocalRepository(get()) }
+            // now initialized
+            testInitialized = true
 
-            // declare the local DB singleton object - used as data source for the repository
-            // note: LocalDB.createRemindersDao returns a DAO with interface RemindersDao
-            //       ... the DAO is needed by the repo (where it is injected, see "get()", above)
-            //
-            // object LocalDB {
-            //
-            //    /**
-            //     * static method that creates a reminder class and returns the DAO of the reminder
-            //     */
-            //    fun createRemindersDao(context: Context): RemindersDao {
-            //        return Room.databaseBuilder(
-            //            context.applicationContext,
-            //            RemindersDatabase::class.java, "locationReminders.db"
-            //        ).build().reminderDao()
-            //    }
-            single { LocalDB.createRemindersDao(ApplicationProvider.getApplicationContext()) }
-        }
+        }  // if(testInitialized)
 
-        // instantiate viewModels, repos and DBs and inject them as services into consuming classes
-        // ... using KOIN framework (as "service locator"): https://insert-koin.io/
-        startKoin {
-            androidContext(ApplicationProvider.getApplicationContext())
-            modules(listOf(myModule))
-        }
 
-    }  // setup()
+        // run before each test ----------------------------------------
 
-    @Test
-    fun validateEnteredData_ensuresTitleAndDescriptionNotEmpty() {
-
-        // given a viewModel and some reminder data item with empty title and empty description
-        val _viewModel: SaveReminderViewModel by inject()
-        val reminderData = ReminderDataItem(
-            "",
-            "",
-            "",
+        // pre-initialize reminderData (valid)
+        reminderData = ReminderDataItem(
+            "test title",
+            "test description",
+            "test location",
             1.0,
             2.0,
         )
 
+    }  // setupClass()
+
+
+    @Test
+    fun `validateEnteredData returns false if title is empty`() {
+
+        // given access to the viewModel and the PRIVATE method to be tested via reflection
+        //     see: https://medium.com/mindorks/how-to-unit-test-private-methods-in-java-and-kotlin-d3cae49dccd
+        privateTestFun = _viewModel.javaClass.getDeclaredMethod("validateEnteredData", reminderData.javaClass)
+        privateTestFun.setAccessible(true)
+
         // when validating the data item
-        val result = _viewModel.validateAndSaveReminder(reminderData)
+        reminderData.title = null
 
         // then false should be returned
-        Assert.assertEquals(false, result)
+        Assert.assertEquals(false, privateTestFun(_viewModel, reminderData))
+
     }
 
-    @After
-    fun stopKoinAfterTest() = stopKoin()
+//    @Test
+//    fun `validateEnteredData returns false if location is empty`() {
+//
+//        // given a viewModel
+//        val _viewModel: SaveReminderViewModel by inject()
+//
+//        // when validating the data item
+//        val reminderData = ReminderDataItem(
+//            "hello",
+//            "irrelevant",
+//            "",
+//            1.0,
+//            2.0,
+//        )
+//
+//        // then false should be returned
+//        Assert.assertEquals(false, _viewModel.validateEnteredData(reminderData))
+//    }
+//
+//    @Test
+//    fun `validateEnteredData returns true if neither title nor location are empty`() {
+//
+//        // given a viewModel
+//        val _viewModel: SaveReminderViewModel by inject()
+//
+//        // when validating the data item
+//        val reminderData = ReminderDataItem(
+//            "hello",
+//            "",
+//            "mystery",
+//            1.0,
+//            2.0,
+//        )
+//
+//        // then true should be returned
+//        Assert.assertEquals(true, _viewModel.validateEnteredData(reminderData))
+//    }
 
 }
