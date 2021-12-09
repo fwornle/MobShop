d259ccf995e53de7bb1a5c71f93aaf46a0ab26ec
fwornle <fwornle@yahoo.co.uk>
2021-12-09 20:16:15 +0100

    Repro test completed

--- starter/app/src/androidTest/java/com/udacity/project4/locationreminders/data/local/RemindersLocalRepositoryTest.kt
+++ starter/app/src/androidTest/java/com/udacity/project4/locationreminders/data/local/RemindersLocalRepositoryTest.kt
@@ -1,22 +1,16 @@
 package com.udacity.project4.locationreminders.data.local
 
-import androidx.arch.core.executor.testing.InstantTaskExecutorRule
-import androidx.room.Room
-import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.MediumTest
+import com.udacity.project4.locationreminders.data.ReminderDataSource
 import com.udacity.project4.locationreminders.data.dto.ReminderDTO
 import com.udacity.project4.locationreminders.data.dto.Result
-import com.udacity.project4.locationreminders.reminderslist.ReminderDataItem
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.test.runBlockingTest
+import org.hamcrest.CoreMatchers
 import org.hamcrest.CoreMatchers.`is`
-import org.hamcrest.CoreMatchers.instanceOf
 import org.hamcrest.MatcherAssert.assertThat
-import org.junit.After
 import org.junit.Before
-import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 import java.util.*
@@ -27,53 +21,199 @@ import java.util.*
 @MediumTest
 class RemindersLocalRepositoryTest {
 
-    // generate some test database items (location reminders)
-    private var reminderDto1 = ReminderDTO(
-        "test title 1",
-        "test description 1",
-        "test location 1",
-        1.0,
-        1.0,
-        UUID.randomUUID().toString(),
-    )
-    private var reminderDto2 = ReminderDTO(
-        "test title 2",
-        "test description 2",
-        "test location 2",
-        2.0,
-        2.0,
-        UUID.randomUUID().toString(),
-    )
-    private var reminderDto3 = ReminderDTO(
-        "test title 3",
-        "test description 3",
-        "test location 3",
-        3.0,
-        3.0,
-        UUID.randomUUID().toString(),
-    )
+    // test data for (fake) DB
+    private lateinit var reminderDtoList: MutableList<ReminderDTO>
+    private lateinit var newReminderDTO: ReminderDTO
 
-    // define fake "local DB"
-    private var localReminders: List<ReminderDTO> = listOf(
-        reminderDto1,
-        reminderDto2,
-        reminderDto3,
-    )
+    // fake data source (repo)
+    private lateinit var reminderRepo: ReminderDataSource
 
-    // declare fake data source (initialized anew prior to running each new test - @Before)
-    private lateinit var reminderLocalDataSource: RemindersDao
+    @Before
+    fun setUp() {
 
-    // declare repository (= class under test)
-    private lateinit var reminderRepository: RemindersLocalRepository
+        // generate some test database items (location reminders)
+        reminderDtoList = mutableListOf<ReminderDTO>()
 
-//    @Before
-//    fun createRepository() {
-//        reminderLocalDataSource = // todo: room inMemory DB (localReminders.toMutableList())
-//        reminderRepository = RemindersLocalRepository(
-//            reminderLocalDataSource,
-//            Dispatchers.Unconfined,
-//        )
-//    }
+        // generate some test data
+        for (idx in 0..19) {
+            reminderDtoList.add(
+                ReminderDTO(
+                    "test title $idx",
+                    "test description $idx",
+                    "test location $idx",
+                    idx.toDouble(),
+                    idx.toDouble(),
+                    UUID.randomUUID().toString(),
+                )
+            )
+        }
 
+        // initialize a new reminder for 'saveReminder' test
+        newReminderDTO = ReminderDTO(
+            "a new title",
+            "a new test description",
+            "a new test location",
+            42.0,
+            242.0,
+            UUID.randomUUID().toString(),
+        )
+
+        // get a fresh fake data source (repository)
+        reminderRepo = FakeDataSource(reminderDtoList)
+
+    }  // setUp()
+
+
+    /*
+     * to be tested: repo class
+     *
+     *  interface ReminderDataSource {
+     *      suspend fun getReminders(): Result<List<ReminderDTO>>
+     *      suspend fun saveReminder(reminder: ReminderDTO)
+     *      suspend fun getReminder(id: String): Result<ReminderDTO>
+     *      suspend fun deleteAllReminders()
+     *  }
+     *
+     */
+
+    // test repo interface method 'getReminders'
+    @Test
+    @Suppress("UNCHECKED_CAST")
+    fun repository_getReminders() = runBlockingTest {
+
+        // fetch reminders from (fake) repo
+        val result = reminderRepo.getReminders()
+
+        when (result) {
+            is Result.Success<*> -> {
+
+                // check all data records
+                (result.data as List<ReminderDTO>).mapIndexed { idx, reminder ->
+                    // check for equality
+                    assertThat(reminder, CoreMatchers.notNullValue())
+                    assertThat(reminder.id, `is`(reminderDtoList[idx].id))
+                    assertThat(reminder.title, `is`(reminderDtoList[idx].title))
+                    assertThat(reminder.description, `is`(reminderDtoList[idx].description))
+                    assertThat(reminder.location, `is`(reminderDtoList[idx].location))
+                    assertThat(reminder.latitude, `is`(reminderDtoList[idx].latitude))
+                    assertThat(reminder.longitude, `is`(reminderDtoList[idx].longitude))
+
+                }
+
+            }
+            is Result.Error ->
+                assertThat(result.message,
+                    `is`("Could not fetch reminders from (fake) local storage."))
+        }
+
+    }
+
+
+    // test repo interface method 'getReminder' - existing reminder
+    @Test
+    @Suppress("UNCHECKED_CAST")
+    fun repository_getReminderReminderExistsInDB() = runBlockingTest {
+
+        // fetch first reminderer from (fake) repo
+        val idx = 4
+        val result = reminderRepo.getReminder(reminderDtoList[idx].id)
+
+        when (result) {
+            is Result.Success<*> -> {
+
+                // check all data records
+                (result.data as ReminderDTO).let {
+                    // check for equality
+                    assertThat(it, CoreMatchers.notNullValue())
+                    assertThat(it.id, `is`(reminderDtoList[idx].id))
+                    assertThat(it.title, `is`(reminderDtoList[idx].title))
+                    assertThat(it.description, `is`(reminderDtoList[idx].description))
+                    assertThat(it.location, `is`(reminderDtoList[idx].location))
+                    assertThat(it.latitude, `is`(reminderDtoList[idx].latitude))
+                    assertThat(it.longitude, `is`(reminderDtoList[idx].longitude))
+
+                }
+
+            }
+            is Result.Error ->
+                assertThat(result.message,
+                    `is`("Reminder not found!"))
+        }
+
+    }
+
+
+    // test repo interface method 'getReminder' - non-existing reminder
+    @Test
+    @Suppress("UNCHECKED_CAST")
+    fun repository_getReminderReminderDoesNotExistsInDB() = runBlockingTest {
+
+        // attempt to fetch non-existing reminderer from (fake) repo
+        val nonId = "this-index-does-not-exist-in-DB"
+        when (val result = reminderRepo.getReminder(nonId)) {
+            is Result.Error ->
+                assertThat(result.message,
+                    `is`("Reminder with ID $nonId not found in (fake) local storage."))
+            else -> {
+                assertThat("this should",
+                    `is`("not happen (exception during getReminder)"))
+            }
+        }
+
+    }
+
+
+    // test repo interface method 'deleteAllReminders'
+    @Test
+    @Suppress("UNCHECKED_CAST")
+    fun repository_deleteAllReminders() = runBlockingTest {
+
+        // purge all items from (fake) repo, the read all reminders
+        reminderRepo.deleteAllReminders()
+        val result = reminderRepo.getReminders()
+
+        when (result) {
+            is Result.Success<*> -> {
+                assertThat((result.data as List<ReminderDTO>).size, `is`(0))
+            }
+            is Result.Error ->
+                assertThat("this should", `is`("not happen"))
+        }
+
+    }
+
+
+    // test repo interface method 'saveReminder'
+    @Test
+    @Suppress("UNCHECKED_CAST")
+    fun repository_saveReminder() = runBlockingTest {
+
+        // save new reminder to (fake) repo, then read it back
+        reminderRepo.saveReminder(newReminderDTO)
+        val result = reminderRepo.getReminder(newReminderDTO.id)
+
+        when (result) {
+            is Result.Success<*> -> {
+
+                // check the read back data record
+                (result.data as ReminderDTO).let {
+                    // check for equality
+                    assertThat(it, CoreMatchers.notNullValue())
+                    assertThat(it.id, `is`(newReminderDTO.id))
+                    assertThat(it.title, `is`(newReminderDTO.title))
+                    assertThat(it.description, `is`(newReminderDTO.description))
+                    assertThat(it.location, `is`(newReminderDTO.location))
+                    assertThat(it.latitude, `is`(newReminderDTO.latitude))
+                    assertThat(it.longitude, `is`(newReminderDTO.longitude))
+
+                }
+
+            }
+            is Result.Error ->
+                assertThat(result.message,
+                    `is`("Reminder not found!"))
+        }
+
+    }
 
 }
